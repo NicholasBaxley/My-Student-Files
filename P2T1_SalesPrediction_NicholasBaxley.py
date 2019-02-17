a74f6ab219768eabb98e6b9c65aa404e5aa2a914
NicholasBaxley <47706880+NicholasBaxley@users.noreply.github.com>
2019-02-16 23:42:59 -0500

    Add files via upload

---
+++ P2T1_SalesPrediction_NicholasBaxley.py
@@ -0,0 +1,8 @@
+# Determine the profit of sales
+# 2/16/2019
+# CTI-110 P2T1 - Sales Prediction
+# Nicholas Baxley
+
+sales = float(input('The projected sales: '))
+profit = sales * .23
+print('The profit is', format(profit, ',.2f'))
